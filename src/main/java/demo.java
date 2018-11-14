320ace0e350e2091fcfce2c247893a5d15bd6e83
ty <ty123@qq.com>
2018-11-14 16:44:04 +0800

    Initial commit

---
+++ src/main/java/demo.java
@@ -0,0 +1,14 @@
+/**
+ * @Auther ty
+ * @Date 2018/11/14 16:38
+ * @Version 1.0
+ */
+public class demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+
+        System.out.println("Hello SB World!!!!");
+    }
+
+}
